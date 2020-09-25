dd66369c49e3d43374dcf4c34d81a454cd741175
Rob Christie <robchristie@gmail.com>
2020-09-25 15:07:47 -0600

    Fix url.

--- Formula/forego.rb
+++ Formula/forego.rb
@@ -1,7 +1,7 @@
 class Forego < Formula
   desc "Foreman in Go for Procfile-based application management. Fork by rwc9u"
   homepage "https://github.com/rwc9u/forego"
-  url "https://github.com/rwc9u/forego/releases/tag/0.20.1-proxy-support"
+  url "https://github.com/rwc9u/forego/archive/0.20.1-proxy-support.tar.gz"
   sha256 "9e8464250c71b129a54f1336575825bc016ee98446b8efb0980a8afed97bb9d1"
   license "Apache-2.0"
   head "https://github.com/rwc9u/forego.git", branch: "develop"
